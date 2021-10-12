ddde0fedd7bdfd45c4e1b088f6f8100d42e5705d
Sascha Mayr <mayr.sascha@gmail.com>
2021-10-12 12:30:15 +0200

    Fix the supertrend calculation

--- utils/supertrend.js
+++ utils/supertrend.js
@@ -1,3 +1,6 @@
+import round from 'lodash/round'
+
+// Taken from https://tradingtuitions.com/supertrend-indicator-excel-sheet-with-realtime-buy-sell-signals/
 const supertrend = (data = [], options = { atrPeriods: 10, multiplier: 1.5 }) => {
   return data
   .map((entry, index) => {
@@ -6,49 +9,70 @@ const supertrend = (data = [], options = { atrPeriods: 10, multiplier: 1.5 }) =>
     }
     // Max of (High-Low, High-Close, Low-Close)
     const tr = Math.max(...[entry[2] - entry[3], Math.abs(entry[2] - data[index - 1][4]), Math.abs(entry[3] - data[index - 1][4])]) || 0
-    return [...entry, tr]
+    return [...entry, round(tr, 2)]
   })
   .map((entry, index, array) => {
-    if (index >= options.atrPeriods) {
-      // Calc ATR(SMA)
-      const atrSMA = array
-        .slice(index - options.atrPeriods, index)
-        .map(atrEntry => atrEntry[atrEntry.length - 1])
-        .reduce((previous, next) => previous + next, 0) / options.atrPeriods
-      // Calc Basic upperband
-      const basicUpperband = (entry[2] + entry[3]) / 2 + options.multiplier * atrSMA
-      // Calc Basic lowerband
-      const basicLowerband = (entry[2] + entry[3]) / 2 - options.multiplier * atrSMA
+    if (index < options.atrPeriods) { return entry; }
 
-      return [...entry, atrSMA, basicUpperband, basicLowerband]
-    }
-    return entry
-  })
-  .map((entry, index) => {
+    // Calc ATR(SMA)
+    const atrSMA = array
+      .slice(index + 1 - options.atrPeriods, index + 1)
+      .map(atrEntry => atrEntry[atrEntry.length - 1])
+      .reduce((previous, next) => previous + next, 0) / options.atrPeriods
+    const roundedAtrSMA = round(atrSMA, 2)
+
+    const basicUpperband = (entry[2] + entry[3]) / 2 + options.multiplier * atrSMA
+    const roundedBasicUpperband = round(basicUpperband, 2)
+    const basicLowerband = (entry[2] + entry[3]) / 2 - options.multiplier * atrSMA
+    const roundedBasicLowerband = round(basicLowerband, 2)
+
+    let newEntry = [...entry, roundedAtrSMA, roundedBasicUpperband, roundedBasicLowerband]
+    // The final upperband and the final lowerband equal the basic upperband and the basic lowerband for the first entry in the ATR period
     if (index === options.atrPeriods) {
-      return [...entry, entry[7], entry[8]]
+      newEntry.push(roundedBasicUpperband, roundedBasicLowerband)
     }
-    return entry
+
+    return newEntry
   })
   .reduce((previousResult, currentEntry, index) => {
-    if (index > options.atrPeriods) {
-      // IF( (Current Basic Upperband  < Previous Final Upperband) and (Previous Close > Previous Final Upperband)) Then (Current Basic Upperband) ELSE Previous Final Upperband)
-      const previousFinalUpperband = previousResult[index - 1][7]
-      const finalUpperband = currentEntry[7] < previousFinalUpperband && previousResult[index -1][4] > previousFinalUpperband ? currentEntry[7] : previousFinalUpperband
-      // IF( (Current Basic Lowerband  > Previous Final Lowerband) and (Previous Close < Previous Final Lowerband)) Then (Current Basic Lowerband) ELSE Previous Final Lowerband)
-      const previousFinalLowerband = previousResult[index - 1][8]
-      const finalLowerband = currentEntry[8] > previousFinalLowerband && previousResult[index -1][4] > previousFinalLowerband ? currentEntry[8] : previousFinalLowerband
-      return [...previousResult, [...currentEntry, finalUpperband, finalLowerband]]
-    }
-    return [...previousResult, currentEntry]
+    if (index <= options.atrPeriods) { return [...previousResult, currentEntry] }
+
+    const previousClose = previousResult[index -1][4]
+
+    const previousFinalUpperband = previousResult[index - 1][9]
+    const currentBasicUpperband = currentEntry[7]
+    const finalUpperband = (currentBasicUpperband < previousFinalUpperband || previousClose > previousFinalUpperband) ? currentBasicUpperband : previousFinalUpperband
+
+    const previousFinalLowerband = previousResult[index - 1][10]
+    const currentBasicLowerband = currentEntry[8]
+    const finalLowerband = (currentBasicLowerband > previousFinalLowerband || previousClose < previousFinalLowerband) ? currentBasicLowerband : previousFinalLowerband
+
+    return [...previousResult, [...currentEntry, finalUpperband, finalLowerband]]
   }, [])
-  .map((entry, index) => {
-    if (index > options.atrPeriods) {
-      // IF(Current Close <= Current Final Upperband ) Then Current Final Upperband ELSE Current  Final Lowerband
-      const supertrend = entry[4] <= entry[7] ? entry[7] : entry[8]
-      return entry[4] < supertrend ? 'sell' : 'buy'
+  .map((entry, index, array) => {
+    if (index < options.atrPeriods) { return '' }
+
+    const previousSuperTrend = array[index - 1][11]
+    const previousFinalUpperband = array[index - 1][9]
+    const previousFinalLowerband = array[index - 1][10]
+    const currentClose = entry[4]
+    const currentFinalUpperband = entry[9]
+    const currentFinalLowerband = entry[10]
+
+    let supertrendIndicator;
+    if (previousSuperTrend === previousFinalUpperband && currentClose <= currentFinalUpperband) {
+      supertrendIndicator = currentFinalUpperband
+    } else if (previousSuperTrend === previousFinalUpperband && currentClose > currentFinalUpperband) {
+      supertrendIndicator = currentFinalLowerband
+    } else if (previousSuperTrend === previousFinalLowerband && currentClose >= currentFinalUpperband) {
+      supertrendIndicator = currentFinalLowerband
+    } else if (previousSuperTrend === previousFinalLowerband && currentClose < currentFinalLowerband) {
+      supertrendIndicator = currentFinalUpperband
     }
-    return ''
+
+    entry.push(supertrendIndicator)
+
+    return entry[4] < supertrendIndicator ? 'sell' : 'buy'
   })
 }
 
