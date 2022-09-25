e877dccc80d0c949c0b82f8f0e9b3ab9031cad15
Pascal Mayr <pascalmayr2@gmail.com>
2022-09-25 21:51:48 +0200

    add darkmode switcher

--- components/Header.js
+++ components/Header.js
@@ -3,9 +3,12 @@ import { Layout, Menu } from 'antd'
 import Logo from './Logo'
 
 import headerStyles from '../styles/header.module.less'
+import DarkModeSwitch from './DarkModeSwitch';
+import useDarkMode from '../hooks/usedarkmode';
 
 const Header = () => {
   const { Header: AntHeader } = Layout;
+  const [darkMode, setDarkMode] = useDarkMode()
 
   return (
     <AntHeader className={headerStyles.header}>
@@ -13,7 +16,7 @@ const Header = () => {
         <Menu.Item key="logo" className={headerStyles.logo} data-id="logo">
           <Logo />
         </Menu.Item>
-        {/* <Menu.Item key="dark-mode"><DarkModeSwitch darkMode={darkMode} setDarkMode={setDarkMode}/></Menu.Item> */}
+        <Menu.Item key="dark-mode" className={headerStyles.darkModeSwitchItem}><DarkModeSwitch darkMode={darkMode} setDarkMode={setDarkMode} /></Menu.Item>
       </Menu>
     </AntHeader>
   );
