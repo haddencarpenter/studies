d3fc1e76be4d36955f38869c1b100d5de854cfb6
Sascha Mayr <mayr.sascha@gmail.com>
2022-11-26 23:55:03 +0100

    Use the Satoshi font

--- pages/_document.js
+++ pages/_document.js
@@ -6,9 +6,8 @@ class MyDocument extends Document {
       <Html lang="en">
         <Head>
           <meta charSet="UTF-8"/>
-          <link rel="preconnect" href="https://fonts.googleapis.com"/>
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true"/>
-          <link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:wght@400;700&display=swap" rel="stylesheet"></link>
+          <link rel="preconnect" href="https://fonts.cdnfonts.com"/>
+          <link href="https://fonts.cdnfonts.com/css/satoshi" rel="stylesheet"></link>
 
           <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
           <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
