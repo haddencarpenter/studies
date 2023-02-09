2f9822208f79665d91d389fd40ef463b8d51a43a
Sascha Mayr <mayr.sascha@gmail.com>
2023-02-09 15:11:56 +0700

    Fix table style

--- hooks/useVirtualTable.js
+++ hooks/useVirtualTable.js
@@ -2,8 +2,8 @@ import { useMemo } from 'react'
 import { VList } from 'virtuallist-antd'
 
 const useVirtualTable = () => {
-  // The table rows are 56px high.
-  const tableHeight = 9 * 56;
+  // The table rows are 57px high.
+  const tableHeight = 10 * 57;
   const vComponents = useMemo(() => {
 		return VList({
 			height: tableHeight
