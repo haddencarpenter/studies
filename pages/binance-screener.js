361892c467811a4ac66d5edc752de299c2590291
Sascha Mayr <mayr.sascha@gmail.com>
2023-03-05 01:24:10 +0100

    Fix binance screener style explainer

--- pages/binance-screener.js
+++ pages/binance-screener.js
@@ -28,7 +28,7 @@ export default function BinanceScreener({ coinsData, appData, exchangeData }) {
 
 Here are some features and strategies to consider:
 
-# Binance Screener Capabilities
+## Binance Screener Capabilities
 
 - Daily updates for all Binance-listed coins' trend status
 - Sorting options based on marketcap and derivatives market availability
