ae5d23a855cb2561c81f9211f20b04197b66d965
Sascha Mayr <mayr.sascha@gmail.com>
2023-05-04 15:17:54 +0200

    Try ISR on demand

---
+++ pages/api/strapi-revalidate.js
@@ -0,0 +1,30 @@
+import slugify from "slugify"
+
+const strapiModels = ["category", "coin", "page"]
+
+export default async function strapiRevalidate(req, res) {
+  const entry = req.body.entry
+
+  if (req.query.secret !== process.env.STRAPI_REVALIDATE_SECRET) {
+    return res.status(401).json({ message: 'Invalid token' })
+  }
+
+  if (strapiModels.includes(entry.model)) {
+    let revalidatePath;
+    if (entry.model === "category") {
+      revalidatePath = `/category/${slugify(entry.name)}`
+    } else if (entry.model === "coin") {
+      revalidatePath = `/coin/${entry.slug}`
+    } else if (entry.model === "page") {
+      revalidatePath = `/`
+    }
+    try {
+      await res.revalidate(revalidatePath)
+      return res.json({ revalidated: true })
+    } catch (err) {
+      return res.status(500).send('Error revalidating.')
+    }
+  } else {
+    return res.status(500).send('Error revalidating.')
+  }
+}
