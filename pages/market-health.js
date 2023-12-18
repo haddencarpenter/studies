800d0897022488b7c41dd87688a2f0323ead21ca
Sascha Mayr <mayr.sascha@gmail.com>
2023-12-18 14:36:17 +0700

    Market health page use ws

--- pages/market-health.js
+++ pages/market-health.js
@@ -1,11 +1,8 @@
 import { Layout, Card } from 'antd';
-import endOfYesterday from 'date-fns/endOfYesterday';
-import { useContext } from 'react'
+import { useContext, useCallback, useEffect, useState } from 'react'
 import subDays from 'date-fns/subDays';
 import addDays from 'date-fns/addDays';
 import Head from 'next/head';
-import groupBy from 'lodash/groupBy.js';
-import countBy from 'lodash/countBy.js';
 import { gql } from '@urql/core'
 
 import { DarkModeContext } from '../layouts/screener.js';
@@ -13,19 +10,68 @@ import baseStyles from '../styles/base.module.less'
 import globalData from '../lib/globalData';
 import PageHeader from '../components/PageHeader'
 import MarketHealthChart from '../components/MarketHealthChart';
-import { signals, SUPERTREND_FLAVOR } from 'coinrotator-utils/variables.mjs'
 import useBreakPoint from '../hooks/useBreakPoint';
-import supersupertrend from 'coinrotator-utils/supersupertrend.mjs';
-import prisma from "../lib/prisma.mjs"
 import strapi from '../utils/strapi';
+import useSocketStore from '../hooks/useSocketStore';
 
 import styles from "../styles/market-health.module.less"
 
 const { Content } = Layout;
 
-export default function MarketHealth({ historicDailySuperSuperTrends, appData, pageData }) {
+export default function MarketHealth({ appData, pageData }) {
+  const dateFormatter = new Intl.DateTimeFormat([], { month: 'short', day: 'numeric' })
   const [darkMode] = useContext(DarkModeContext);
   const screens = useBreakPoint();
+  const socket = useSocketStore(state => state.socket)
+  const [trends, setTrends] = useState(null)
+  const fetchTrends = useCallback(() => {
+    if (socket) {
+      socket.emit('get_historical_trends')
+    }
+  }, [socket])
+  useEffect(() => {
+    console.log('useeffect fetch trends')
+    fetchTrends()
+  }, [fetchTrends])
+  useEffect(() => {
+    if (socket) {
+      socket.on('historical_trends', (trends) => setTrends(trends))
+      socket.on('new_trends', fetchTrends)
+    }
+    return () => {
+      if (socket) {
+        socket.off('historical_trends')
+        socket.off('new_trends')
+      }
+    }
+  }, [socket, fetchTrends])
+  let historicDailySuperSuperTrends = []
+  if (trends) {
+    for (const [_quoteSymbol, { historical }] of Object.entries(trends)) {
+      for (const [date, trend] of Object.entries(historical)) {
+        const matchingDate = historicDailySuperSuperTrends.find((dataPoint) => dataPoint.date === date && dataPoint.trend === trend)
+        if (matchingDate) {
+          matchingDate.amount++
+        } else {
+          historicDailySuperSuperTrends.push({
+            date,
+            amount: 1,
+            trend
+          })
+        }
+      }
+    }
+    const thirtyDaysAgo = subDays(new Date(), 30).getTime()
+    historicDailySuperSuperTrends = historicDailySuperSuperTrends.filter(dataPoint => dataPoint.date > thirtyDaysAgo)
+    historicDailySuperSuperTrends = historicDailySuperSuperTrends.sort((a, b) => a.date - b.date)
+    historicDailySuperSuperTrends = historicDailySuperSuperTrends.map((dataPoint) => {
+      let date = new Date(parseInt(dataPoint.date))
+      date = addDays(date, 1)
+      dataPoint.date = dateFormatter.format(date)
+
+      return dataPoint
+    })
+  }
   return (
     <>
       <Head>
@@ -48,13 +94,6 @@ export default function MarketHealth({ historicDailySuperSuperTrends, appData, p
 
 export async function getStaticProps() {
   const appData = await globalData();
-  const yesterday = endOfYesterday();
-  const coinQuery = {
-    orderBy: { marketCapRank: 'asc' },
-    select: {
-      id: true,
-    }
-  }
   let { data } = await strapi.query(
     gql`
       query Pages($slug: String) {
@@ -75,54 +114,10 @@ export async function getStaticProps() {
     }
   )
   data = data.pages.data[0].attributes
-  let coins
-  if (process.env.NODE_ENV === 'development') {
-    coins = await prisma.coin.findMany({...coinQuery, take: 20})
-  } else {
-    coins = await prisma.coin.findMany({...coinQuery, take: 1000})
-  }
-  let historicDailySuperSuperTrends = []
-  const dateFormatter = new Intl.DateTimeFormat([], { month: 'short', day: 'numeric' })
-  for (let i = 0, date = yesterday; i < 30; i++) {
-    let superTrends = await prisma.superTrend.findMany({
-      select: {
-        coinId: true,
-        trend: true,
-        date: true
-      },
-      where: {
-        coinId: {
-          in: coins.map(c => c.id)
-        },
-        date: date,
-        flavor: SUPERTREND_FLAVOR.coinrotator,
-        weekly: false
-      }
-    })
-    superTrends = groupBy(superTrends, 'coinId')
-    const supersupertrends = Object.values(superTrends).flatMap((trends) => supersupertrend(trends.map(t => t.trend)))
-
-    for (const trend of [signals.buy, signals.hodl, signals.sell]) {
-      historicDailySuperSuperTrends.push({
-        date,
-        amount: countBy(supersupertrends, (st) => st === trend).true,
-        trend
-      })
-    }
-
-    date = subDays(date, 1)
-  }
-  historicDailySuperSuperTrends = historicDailySuperSuperTrends.reverse()
-  historicDailySuperSuperTrends = historicDailySuperSuperTrends.map((historicalDataPoint) => {
-    historicalDataPoint.date = dateFormatter.format(addDays(historicalDataPoint.date, 1))
-
-    return historicalDataPoint
-  })
 
   return {
     props: {
       appData,
-      historicDailySuperSuperTrends,
       pageData: data
     }
   };
