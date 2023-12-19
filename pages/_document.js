bcf5d589f02cb46911a4b61d414325aa415c9565
Sascha Mayr <mayr.sascha@gmail.com>
2023-12-19 17:17:27 +0700

    Timing logs

--- pages/_document.js
+++ pages/_document.js
@@ -16,6 +16,10 @@ export default function Document() {
         <meta name="theme-color" content="#ffffff"></meta>
       </Head>
       <body>
+        <script>
+          const date = new Date();
+          console.log(date.toLocaleTimeString(), date.getMilliseconds());
+        </script>
         <Main />
         <NextScript />
       </body>
