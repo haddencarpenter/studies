9c3057c2da0447c5f48b670f7e9618b4e92ad8dd
Sascha Mayr <mayr.sascha@gmail.com>
2024-05-21 20:05:06 +0200

    Real medium.com key pass link

--- components/gating/NoKeyPass.js
+++ components/gating/NoKeyPass.js
@@ -7,7 +7,7 @@ const NoKeyPass = () => {
     <div>
       <span className={gatingStyles.text}>CoinRotator Key Pass hasn&apos;t been found in this wallet.</span>
       <div className={gatingStyles.buttonWrapper}>
-        <a href="https://medium.com" rel="noopener noreferrer" target="_blank">
+        <a href="https://coinrotator.medium.com/c126a79ead6c" rel="noopener noreferrer" target="_blank">
           <Button type="primary">Get your Key Pass</Button>
         </a>
       </div>
