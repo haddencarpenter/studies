7d3d8a277552f441d7dcae7a97650633533c2b67
Sascha Mayr <mayr.sascha@gmail.com>
2025-04-25 22:07:53 +0200

    Toady meta title and desc

--- pages/toady.js
+++ pages/toady.js
@@ -1,18 +1,56 @@
+import Head from 'next/head';
+import { gql } from '@urql/core';
 import globalData from '../lib/globalData'
-import ToadyComponent from '../components/Toady'; // Restore original component import
+import ToadyComponent from '../components/Toady';
+import strapi from '../utils/strapi';
 
 export async function getStaticProps() {
   const appData = await globalData()
+
+  let pageQuery = null;
+  try {
+    pageQuery = await strapi.query(
+      gql`
+        query Pages($slug: String) {
+          pages(filters: {slug: {eq: $slug}}) {
+            data {
+              attributes {
+                title
+                metaTitle
+                metaDescription
+              }
+            }
+          }
+        }
+      `,
+      {
+        slug: 'toady',
+      }
+    );
+  } catch (error) {
+    console.error("Error fetching page data from Strapi:", error);
+    // Handle error appropriately, maybe return default pageData
+  }
+
+  const pageData = pageQuery?.data?.pages?.data[0]?.attributes || {};
+
   return {
     props: {
       appData,
+      pageData,
     },
   }
 }
 
-const ToadyPage = () => {
+const ToadyPage = ({ pageData }) => {
   return (
-    <ToadyComponent isActive={true} />
+    <>
+      <Head>
+        <title key="title">{pageData?.metaTitle}</title>
+        <meta name="description" key="description" content={pageData?.metaDescription} />
+      </Head>
+      <ToadyComponent isActive={true} />
+    </>
   );
 };
 
