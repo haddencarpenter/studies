710076614c8e84da5a15dd0666ca3b54391f2c38
alphalabs12 <labsalpha28@gmail.com>
2025-06-21 01:52:11 +0500

    Fix COOP headers for Web3Auth - use same-origin-allow-popups and add vercel.json

--- next.config.js
+++ next.config.js
@@ -56,11 +56,7 @@ let moduleExports = {
         headers: [
           {
             key: 'Cross-Origin-Opener-Policy',
-            value: 'unsafe-none',
-          },
-          {
-            key: 'Cross-Origin-Embedder-Policy',
-            value: 'unsafe-none',
+            value: 'same-origin-allow-popups',
           },
         ],
       }
