0e719cdb9e6b32638be4376d7c7e58abc441ab76
coinrotatordev <info@coinrotator.app>
2025-08-26 18:06:47 +0200

    Link to shumi trading page

--- components/Banner.js
+++ components/Banner.js
@@ -2,11 +2,10 @@ import headerStyles from '../styles/header.module.less'
 import { Button } from 'antd'
 
 const Banner = () => {
-  return null;
   return (
     <div className={headerStyles.banner}>
-      <span>$SHUMI launching on Virtuals August 22</span>
-      <a href="https://app.virtuals.io/geneses/7272" target="_blank"><Button className={headerStyles.bannerButton} type="primary">Join Launch</Button></a>
+      <span>$SHUMI now trading on Virtuals</span>
+      <a href="https://app.virtuals.io/virtuals/36228" target="_blank"><Button className={headerStyles.bannerButton} type="primary">Join Launch</Button></a>
     </div>
   );
 }
